f48c3ab9d4cc77b94afb855a678c71da36006c51
mastersenpai05 <pampangavergil0405@gmail.com>
2021-12-26 18:09:02 +0800

    Note_20_Pro: adapy dynamic partition device tree

--- omni_NOTE_20_PRO.mk
+++ omni_NOTE_20_PRO.mk
@@ -6,16 +6,11 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from NOTE_20_PRO device
-$(call inherit-product, device/cubot/NOTE_20_PRO/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := NOTE_20_PRO
